3aaae5ebf6e7f12a805228b407065cfb91fbfcbb
Jonathan Hall <flimzy@flimzy.com>
2022-03-10 17:48:53 +0100

    Smarter type/schema conversion

--- internal/types.go
+++ internal/types.go
@@ -3,6 +3,8 @@ package internal
 import (
 	"path/filepath"
 	"reflect"
+	"regexp"
+	"strings"
 
 	"github.com/invopop/gobl/schema"
 )
@@ -12,6 +14,7 @@ func FindType(term string) schema.ID {
 }
 
 func findType(types map[reflect.Type]schema.ID, term string) schema.ID {
+	schema := toSchema(term)
 	for typ, id := range types {
 		if term == string(id) {
 			return id
@@ -22,6 +25,32 @@ func findType(types map[reflect.Type]schema.ID, term string) schema.ID {
 		if term == filepath.Base(typ.PkgPath())+"."+typ.Name() {
 			return id
 		}
+		if strings.HasSuffix(string(id), schema) {
+			return id
+		}
 	}
 	return ""
+}
+
+var cap = regexp.MustCompile("([A-Z])")
+var allCaps = regexp.MustCompile("[A-Z]{2,}")
+
+func toSchema(term string) string {
+	if strings.HasPrefix(term, "http://") || strings.HasPrefix(term, "https://") {
+		return term
+	}
+	if strings.Contains(term, ".") {
+		parts := strings.Split(term, ".")
+		for i, part := range parts {
+			parts[i] = strings.TrimPrefix(toSchema(part), "-")
+		}
+		return "/" + strings.Join(parts, "/")
+	}
+	for _, match := range allCaps.FindAllString(term, -1) {
+		match = match[:len(match)-1]
+		new := strings.ToLower(match)
+		term = strings.Replace(term, match, new, 1)
+	}
+	term = cap.ReplaceAllString(term, "-${1}")
+	return strings.ToLower(term)
 }
