bcd4d5c8ea565d31a4a658b5d4ecef4b601ed344
Jonathan Hall <flimzy@flimzy.com>
2022-04-21 15:38:29 +0200

    Launch the Hello World app in a web worker

--- wasm/index.js
+++ wasm/index.js
@@ -1,14 +1,7 @@
-if (!WebAssembly.instantiateStreaming) {
-    WebAssembly.instantiateStreaming = async (resp, importObject) => {
-        const source = await (await resp).arrayBuffer();
-        return await WebAssembly.instantiate(source, importObject);
-    };
-}
+const worker = new window.Worker("worker.js");
 
-const go = new Go();
-WebAssembly
-    .instantiateStreaming(fetch("main.wasm"), go.importObject)
-    .then((result) => {
-        go.run(result.instance);
-    });
+worker.onmessage = (event) => {
+    console.log(event);
+};
+
 console.log("loaded");
