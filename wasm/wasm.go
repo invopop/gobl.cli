6fa6e704c14e6cc8143e1974507588fc38e3d068
Jonathan Hall <flimzy@flimzy.com>
2022-04-25 15:17:34 +0200

    Pass requests to gobl, and send responses

--- wasm/wasm.go
+++ wasm/wasm.go
@@ -1,21 +1,44 @@
-//go:build js
-
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"syscall/js"
+
+	"github.com/invopop/gobl.cli/internal"
 )
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	r, w := io.Pipe()
 	js.Global().Call("addEventListener", "message", js.FuncOf(func(_ js.Value, args []js.Value) interface{} {
-		fmt.Println("got a message", args)
+		jsonEvent := js.Global().Get("JSON").Call("stringify", args[0].Get("data")).String()
+		fmt.Println("got a message", jsonEvent)
+		_, _ = fmt.Fprintln(w, jsonEvent)
 		return nil
 	}))
+
 	js.Global().Call("postMessage", map[string]interface{}{"ready": true})
+
 	fmt.Println("waiting")
-	c := make(chan struct{})
-	<-c
+	for result := range internal.Bulk(context.TODO(), r) {
+		response := js.Global().Get("Object").New()
+		if result.ReqID != "" {
+			response.Set("req_id", result.ReqID)
+		}
+		response.Set("seq_id", result.SeqID)
+		if len(result.Payload) > 0 {
+			response.Set("payload", string(result.Payload))
+		}
+		if result.Error != "" {
+			response.Set("error", result.Error)
+		}
+		if result.IsFinal {
+			response.Set("is_final", true)
+		}
+		js.Global().Call("postMessage", response)
+	}
 	fmt.Println("exiting")
 }
