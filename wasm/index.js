e5731d0d97da532c663f561ff75483f3486a657d
Jonathan Hall <flimzy@flimzy.com>
2022-04-26 10:25:55 +0200

    Create a gobl JS module

--- wasm/index.js
+++ wasm/index.js
@@ -1,29 +1,3 @@
-var ready = false;
-var queue = [];
+import { keygen } from "./gobl.js";
 
-const worker = new window.Worker("worker.js");
-
-worker.onmessage = (event) => {
-    if (event.data && event.data.ready) {
-        console.log("worker is ready");
-        ready = true;
-        for (var i = 0; i < queue.length; i++) {
-            worker.postMessage(queue[i]);
-        }
-        return;
-    }
-    console.log(event);
-};
-
-console.log("loaded");
-
-function sendMessage(msg) {
-    if (!ready) {
-        console.log("not ready, queueing")
-        queue.push(msg);
-        return;
-    }
-    worker.postMessage(msg);
-}
-
-sendMessage("foo");
+keygen();
