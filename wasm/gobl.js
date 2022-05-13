ce24b789877be23c66f66cb66890382a5c781014
Jonathan Hall <flimzy@flimzy.com>
2022-05-13 10:10:50 +0200

    Reformat files

--- wasm/gobl.js
+++ wasm/gobl.js
@@ -6,69 +6,71 @@ var req_id = 0;
 const worker = new window.Worker("worker.js");
 
 worker.onmessage = (event) => {
-    if (event.data && event.data.ready) {
-        console.log("worker is ready");
-        ready = true;
-        for (var i = 0; i < queue.length; i++) {
-            worker.postMessage(queue[i]);
-        }
-        return true;
+  if (event.data && event.data.ready) {
+    console.log("worker is ready");
+    ready = true;
+    for (var i = 0; i < queue.length; i++) {
+      worker.postMessage(queue[i]);
     }
-    const waiting = inFlight[event.data.req_id];
-    delete inFlight[event.data.req_id];
-    if (!waiting) {
-        console.log("got a response for an unregistered request: " + event.data.req_id);
-        return true;
-    }
-    if (event.data.error) {
-        waiting.reject(event.data.error);
-        return true;
-    }
-    waiting.resolve(event.data.payload);
+    return true;
+  }
+  const waiting = inFlight[event.data.req_id];
+  delete inFlight[event.data.req_id];
+  if (!waiting) {
+    console.log(
+      "got a response for an unregistered request: " + event.data.req_id
+    );
+    return true;
+  }
+  if (event.data.error) {
+    waiting.reject(event.data.error);
+    return true;
+  }
+  waiting.resolve(event.data.payload);
 };
 
 function sendMessage(data) {
-    if (!data.req_id) {
-        data.req_id = `req${++req_id}`;
-    }
-    const promise = new Promise((resolve, reject) => {
-        inFlight[data.req_id] = {
-            "resolve": resolve,
-            "reject": reject,
-        };
-        // resolve("foo");
-    })
-    if (!ready) {
-        queue.push(data);
-        return promise;
-    }
-    worker.postMessage(data);
+  if (!data.req_id) {
+    data.req_id = `req${++req_id}`;
+  }
+  const promise = new Promise((resolve, reject) => {
+    inFlight[data.req_id] = {
+      resolve: resolve,
+      reject: reject,
+    };
+    // resolve("foo");
+  });
+  if (!ready) {
+    queue.push(data);
     return promise;
+  }
+  worker.postMessage(data);
+  return promise;
 }
 
 const keygen = async function () {
-    return sendMessage({ "action": "keygen" })
+  return sendMessage({ action: "keygen" });
 };
 
 const build = async function (opts) {
-    return sendMessage({
-        "action": "build",
-        "payload": opts,
-    });
+  return sendMessage({
+    action: "build",
+    payload: opts,
+  });
 };
 
 const verify = async function (opts) {
-    return sendMessage({
-        "action": "verify",
-        "payload": opts,
-    });
-}
+  return sendMessage({
+    action: "verify",
+    payload: opts,
+  });
+};
 
 const envelop = async function (opts) {
-    return sendMessage({
-        "action": "envelop",
-        "payload": opts,
-    })
-}
+  return sendMessage({
+    action: "envelop",
+    payload: opts,
+  });
+};
 
 export { keygen, build, verify, envelop };
--- wasm/worker.js
+++ wasm/worker.js
@@ -2,19 +2,19 @@ self.importScripts("wasm_exec.js");
 
 // Polyfill instantiateStreaming for browsers missing it
 if (!WebAssembly.instantiateStreaming) {
-    WebAssembly.instantiateStreaming = async (resp, importObject) => {
-        const source = await (await resp).arrayBuffer();
-        return await WebAssembly.instantiate(source, importObject);
-    };
+  WebAssembly.instantiateStreaming = async (resp, importObject) => {
+    const source = await (await resp).arrayBuffer();
+    return await WebAssembly.instantiate(source, importObject);
+  };
 }
 
 // initialize the Go WASM glue
 const go = new self.Go();
-WebAssembly
-    .instantiateStreaming(fetch("main.wasm"), go.importObject)
-    .then((result) => {
-        go.run(result.instance);
-    });
+WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then(
+  (result) => {
+    go.run(result.instance);
+  }
+);
 
 console.log("worker.js");
 // addEventListener('message', async (e) => {
